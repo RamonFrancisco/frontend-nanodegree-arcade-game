f80986a0b2b9071fb61ae60d33abef6b827bd18d
RamonFrancisco <ramon.francisco.silva99@gmail.com>
2018-10-15 23:31:23 -0300

    funcionalidades básicas do jogo

--- js/app.js
+++ js/app.js
@@ -1,37 +1,136 @@
+'use strict';
+class Coordinates {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+};
+
 // Inimigos que nosso jogador deve evitar
-var Enemy = function() {
-    // As variáveis aplicadas a nossas instâncias entram aqui.
-    // Fornecemos uma a você para que possa começcar.
+class Enemy extends Coordinates {
+    constructor( {x = -101, y = 0 } = {} ) {
+        super( x, y)
+        // As variáveis aplicadas a nossas instâncias entram aqui.
+        // Fornecemos uma a você para que possa começcar.
 
-    // A imagem/sprite de nossos inimigos, isso usa um
-    // ajudante que é fornecido para carregar imagens
-    // com facilidade.
-    this.sprite = 'images/enemy-bug.png';
-};
+        // A imagem/sprite de nossos inimigos, isso usa um
+        // ajudante que é fornecido para carregar imagens
+        // com facilidade.
+        this.sprite = 'images/enemy-bug.png';
+        this.speed = Math.random() * 400; 
+        this.positions = [68, 151, 234];
+        this.y = this.positions[Math.floor(Math.random() * this.positions.length)];
+    }
 
-// Atualize a posição do inimigo, método exigido pelo jogo
-// Parâmetro: dt, um delta de tempo entre ticks
-Enemy.prototype.update = function(dt) {
-    // Você deve multiplicar qualquer movimento pelo parâmetro
-    // dt, o que garantirá que o jogo rode na mesma velocidade
-    // em qualquer computador.
-};
+    // Atualize a posição do inimigo, método exigido pelo jogo
+    // Parâmetro: dt, um delta de tempo entre ticks
+    update(dt) {
+        // Você deve multiplicar qualquer movimento pelo parâmetro
+        // dt, o que garantirá que o jogo rode na mesma velocidade
+        // em qualquer computador.
+        if(this.x > 505 ) {
+            this.x = -101;
+            this.y = this.positions[Math.floor(Math.random() * this.positions.length)];
+        }
+        this.x = this.x + this.speed * dt;
+        
+    }
 
-// Desenhe o inimigo na tela, método exigido pelo jogo
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // Desenhe o inimigo na tela, método exigido pelo jogo
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
 
 // Agora, escreva sua própria classe de jogador
 // Esta classe exige um método update(), 
 // um render() e um handleInput().
+class Player extends Coordinates {
+    constructor({x = 202, y = 400} = {}) {
+        super(x, y)
+        this.player = 'images/char-boy.png';
+        this.alive = true;
+        this.live = 3;
+        this.level = 0;
+    }
 
+    resetPosition() {
+        this.y = 400;
+        this.x = 202;
+    }
 
-// Represente seus objetos como instâncias.
-// Coloque todos os objetos inimgos numa array allEnemies
-// Coloque o objeto do jogador numa variável chamada jogador.
+    nextLevel() {
+        if( player.y <= 0) {
+            console.log('level');
+        }
+    }
 
+    isLive(){
+        if( checkCollisions() ) {
+            this.live--;
+        }
+    }
 
+    isDead() {
+        if (this.live === 0 ) {
+            this.alive = false;
+        }
+    }
+
+    
+
+    update() {
+        if( this.y <= -11 ) {
+            setTimeout(() => this.y = 400, 1000 )
+        }
+        this.isLive();
+        this.isDead();
+        this.nextLevel()
+    }
+
+    render() {
+        if( this.alive === true){
+            ctx.drawImage(Resources.get(this.player), this.x, this.y);
+        }
+    }
+
+    handleInput(keyCode) {
+        const eixoX = 101;
+        const eixoY = 83;
+
+        switch(keyCode) {
+            case 'right':
+                (this.x >= 400 ) ? this.x = 404 : this.x += eixoX;  
+                break;
+            case 'left':
+                (this.x <= 0 ) ? this.x = 0 : this.x -= eixoX;
+                break;
+            case 'up':
+                if(!(this.y <= -9)){ this.y -= eixoY };
+                break;
+            case 'down':
+                (this.y >= 400 ) ? this.y = 400 : this.y += eixoY;
+                break;
+        }
+    }
+}
+
+const allEnemies = [
+    new Enemy(),
+    new Enemy(),
+    new Enemy()
+];
+
+const player = new Player(); 
+
+function checkCollisions() {
+    for (const enemy of allEnemies) {
+        if ( Math.floor(enemy.x / 101) === Math.floor(player.x / 101) && Math.floor(enemy.y) === player.y  ) {
+            player.resetPosition();
+            return true;
+        }
+    }
+}
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
 // jogador. método handleInput(). Não é preciso mudar nada.
